6afa654db7741f157ab5879f091be21ea2bc3656
Taeha Kim <xogk1128@naver.com>
2023-05-09 02:52:27 +0900

    week10_hw

---
+++ week10_homework/app/src/main/java/com/example/myapplication/Fragments.kt
@@ -0,0 +1,44 @@
+package com.example.myapplication
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+
+class Dialog : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        isCancelable = false
+
+        return AlertDialog.Builder(requireActivity()).apply {
+            this.setTitle("1971341")
+            this.setMessage("김태하")
+            this.setPositiveButton("OK"){dialog, id -> println("OK") }
+
+        }.create()
+
+    }
+}
+
+class Home : Fragment(R.layout.fragment_layout){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.textView).text = "HomeFragment"
+    }
+}
+
+class Page1 : Fragment(R.layout.fragment_layout){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.textView).text = "Page1Fragment"
+    }
+}
+
+class Page2 : Fragment(R.layout.fragment_layout){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.textView).text = "Page2Fragment"
+    }
+}
--- week10_homework/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ week10_homework/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var appBarConfiguration : AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        val topDest = setOf(R.id.homeFragment, R.id.page1Fragment, R.id.page2Fragment)
+        appBarConfiguration = AppBarConfiguration(topDest, drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
